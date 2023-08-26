91aefc0a1da332956ca71d58facce70e95e038c2
Daniel <dvitorio2@gmail.com>
2023-08-26 16:05:21 -0300

    Construção da seção SEJA UM APOIADOR

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,10 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollToTopBtn = document.querySelector(".scroll-to-top");
-  
-    // Função para verificar a posição da página e mostrar/ocultar o ícone
-    function toggleScrollToTop() {
-      if (window.scrollY > 300) {
-        scrollToTopBtn.classList.add("show");
-      } else {
-        scrollToTopBtn.classList.remove("show");
-      }
-    }
-  
-    // Adiciona o evento de rolagem e executa a função de alternância
-    window.addEventListener("scroll", toggleScrollToTop);
-  
-    // Adiciona o evento de clique para rolar suavemente até o topo
-    scrollToTopBtn.addEventListener("click", function () {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    });
-  
-    // Executa a função de alternância no carregamento inicial da página
-    toggleScrollToTop();
-  });
-  
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'seu_usuario',
+    password: 'sua_senha',
+    database: 'seu_banco_de_dados'
+});
+
+connection.connect();
